dc96041f17d76b43d87dba2781b2a4556eb1e847
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-02-21 22:24:48 -0700

    using match with a minimal main package

--- match/cmd/match/main.go
+++ match/cmd/match/main.go
@@ -1,37 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"os"
-    "bufio"
-    "strings"
+    "os"
+    "github.com/ezebunandu/match"
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: go run main.go <filepath> <match string>")
-		os.Exit(1)
-	}
-
-	filepath := os.Args[1]
-	matchString := os.Args[2]
-
-	file, err := os.Open(filepath)
-	if err != nil {
-		fmt.Printf("Error opening file: %v\n", err)
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.Contains(line, matchString) {
-			fmt.Println(line)
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("Error reading file: %v\n", err)
-	}
+    os.Exit(match.Main())
 }
