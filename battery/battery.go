3fd4ae92e5cadf0a4824337909f3264127748755
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-03-02 10:36:45 -0700

    no op: fmt clean up

--- battery/battery.go
+++ battery/battery.go
@@ -8,21 +8,21 @@ import (
 )
 
 type Status struct {
-    ChargePercent int
+	ChargePercent int
 }
 
 var pmsetOutput = regexp.MustCompile("([0-9]+)%")
 
-func ParsePmsetOutput(text string) ( Status, error){
-    matches := pmsetOutput.FindStringSubmatch(text)
-    if len(matches) < 2 {
-        return Status{}, fmt.Errorf("failed to parse pmset output: %q", text)
-    }
-    charge, err := strconv.Atoi(matches[1])
-    if err != nil {
-        return Status{}, fmt.Errorf("failed to parse charge percentage: %q", matches[1])
-    }
-    return Status{ChargePercent: charge}, nil
+func ParsePmsetOutput(text string) (Status, error) {
+	matches := pmsetOutput.FindStringSubmatch(text)
+	if len(matches) < 2 {
+		return Status{}, fmt.Errorf("failed to parse pmset output: %q", text)
+	}
+	charge, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return Status{}, fmt.Errorf("failed to parse charge percentage: %q", matches[1])
+	}
+	return Status{ChargePercent: charge}, nil
 }
 
 func GetPmsetOutput() (text string, err error) {
